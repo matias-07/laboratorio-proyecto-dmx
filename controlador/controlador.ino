8b2f3bfae8f066ca714fb2ebb8eb40ab9b5c0a1e
Matias <mhuenul@fi.uba.ar>
2019-12-01 21:55:18 -0300

    Se agrega funciones de efecto de luces

--- controlador/controlador.ino
+++ controlador/controlador.ino
@@ -1,52 +1,52 @@
 // ************************ Controlador DMX con Arduino ************************
 //
-// El programa lee la entrada de cuatro botones pulsadores que determinan el
-// canal a controlar (R, G, B o brillo) y de un potenciómetro, que establece
-// el nivel del canal seleccionado.
-//
+// El programa lee la entrada de cuatro botones pulsadores y envía la señal
+// a una luz compatible con el protocolo DMX.
+// Tres de estos botones son utilizados para seleccionar un canal a controlar,
+// (R, G o B) y el otro para seleccionar un efecto de luces predefinido.
+// En el caso de los canales, se utiliza el potenciómetro para controlar
+// la intensidad de las luces, mientras que si es seleccionado el efecto, se lo
+// utiliza para controlar la velocidad del mismo.
+// 
 // *****************************************************************************
 
 #include <DMXSerial.h>
 #include <Bounce2.h>
 
 #define MODO_MASTER     2   // Modo master (controlador)
-#define BOTON_RED       6
-#define BOTON_GREEN     5
-#define BOTON_BLUE      4
-#define BOTON_BRILLO    3
+#define BOTON_EFECTO    6
+#define BOTON_RED       5
+#define BOTON_GREEN     4
+#define BOTON_BLUE      3
 #define PIN_NIVEL       A0  // Potenciómetro
-
 
 // La dirección base es la dirección de la luz DMX, los offsets se le suman
 // para obtener la dirección de la función específica a controlar
 byte direccion_base = 5;
-byte offset_red = 0;
-byte offset_green = 1;
-byte offset_blue = 2;
-byte offset_brillo = 3;
+byte offset_efecto = 6;
+byte offset_red = 1;
+byte offset_green = 2;
+byte offset_blue = 3;
 
 // Variables para guardar el offset y el nivel del potenciómetro actual
 byte offset;
 byte nivel;
+byte offsets[3] = {1, 2, 3};
 
-
+// Botones
 const int debounce_interval = 10;
 Bounce boton_red_bounce = Bounce();
 Bounce boton_green_bounce = Bounce();
 Bounce boton_blue_bounce = Bounce();
-Bounce boton_brillo_bounce = Bounce();
-
+Bounce boton_efecto_bounce = Bounce();
 
 void setup() {
     DMXSerial.init(DMXController, MODO_MASTER);
-    
-    // velocidad de envio de info a la consola
-    Serial.begin(9600);
 
     pinMode(BOTON_RED, INPUT_PULLUP);
     pinMode(BOTON_BLUE, INPUT_PULLUP);
     pinMode(BOTON_GREEN, INPUT_PULLUP);
-    pinMode(BOTON_BRILLO, INPUT_PULLUP);
+    pinMode(BOTON_EFECTO, INPUT_PULLUP);
 
     boton_red_bounce.attach(BOTON_RED);
     boton_red_bounce.interval(debounce_interval);
@@ -54,10 +54,18 @@ void setup() {
     boton_green_bounce.interval(debounce_interval);
     boton_blue_bounce.attach(BOTON_BLUE);
     boton_blue_bounce.interval(debounce_interval);
-    boton_brillo_bounce.attach(BOTON_BRILLO);
-    boton_brillo_bounce.interval(debounce_interval);
+    boton_efecto_bounce.attach(BOTON_EFECTO);
+    boton_efecto_bounce.interval(debounce_interval);
+    DMXSerial.write(direccion_base + 0, 200);  
+}
 
-    DMXSerial.write(direccion_base + offset_brillo, 100);
+void efecto(int velocidad) {
+    for (int i = 0; i < 3; i++) {
+        DMXSerial.write(direccion_base + offsets[i], 200);
+        delay(velocidad);
+        DMXSerial.write(direccion_base + offsets[i], 0);
+        delay(velocidad);
+    }
 }
 
 void pruevaDeMedicionOndaCuadrada_1() {
@@ -77,11 +85,11 @@ void loop() {
     // Obtenemos el valor del potenciómetro (entre 1 y 255)
     nivel = map(analogRead(PIN_NIVEL), 0, 1023, 1, 255);
 
-    // Obtenemos el offset actualmente seleccionado (R, G, B o brillo)
+    // Obtenemos el offset actualmente seleccionado (R, G, B o efecto)
     boton_red_bounce.update();
     boton_green_bounce.update();
     boton_blue_bounce.update();
-    boton_brillo_bounce.update();
+    boton_efecto_bounce.update();
 
     if (boton_red_bounce.fell()) {
         offset = offset_red;
@@ -92,13 +100,20 @@ void loop() {
     if (boton_blue_bounce.fell()) {
         offset = offset_blue;
     }
-    if (boton_brillo_bounce.fell()) {
-        offset = offset_brillo;
+    if (boton_efecto_bounce.fell()) {
+        offset = offset_efecto;
     }
 
-    // Mandamos el valor obtenido al canal seleccionado
-    DMXSerial.write(direccion_base + offset, nivel);
-    
-    // Envio de informacion a la consola
-    Serial.println("rojo: {0};verde: {1};azul: {2};brillo: {3};",offset_red,offset_green,boton_blue_bounce,offset_brillo);
+    if (offset == offset_efecto) {
+      // A niveles altos del potenciómetro, se pasa al control manual.
+      // De esta forma no hay problemas de sincronía para salir del efecto.
+      if (nivel > 240) {
+        offset = offset_red;
+      } else {
+        // Acá el nivel "pasa a ser" la velocidad del efecto.
+        efecto(int(nivel));
+      }
+    } else {
+      DMXSerial.write(direccion_base + offset, nivel);
+    }
 }
